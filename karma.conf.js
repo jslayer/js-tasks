d1e75d48c9ee484cded15bc68388ab5557d66745
Eugene Poltorakov <jslayer@gmail.com>
2013-07-30 19:51:40 +0300

    update karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,36 +1,44 @@
 // Karma configuration
-// Generated on Tue Jul 30 2013 13:29:32 GMT+0300 (EEST)
+// Generated on Tue Jul 30 2013 16:30:35 GMT+0300 (EEST)
+// !!! run on node v0.10.15
 
 module.exports = function(config){
     config.set({
 
         // base path, that will be used to resolve files and exclude
-        basePath       : '',
+        basePath   : '',
 
         // frameworks to use
-        frameworks     : ['mocha'],
+        frameworks : ['mocha', 'chai'],
 
         // list of files / patterns to load in the browser
-        files          : [],
+        files      : [
+            'scripts/*.js',
+            'test/unit/*.js'
+        ],
 
         // list of files to exclude
-        exclude        : [
+        exclude    : [
 
         ],
 
         // test results reporter to use
         // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-        reporters      : ['progress'],
+        reporters  : ['progress'],
 
         // web server port
-        port           : 9876,
+        port       : 9876,
 
         // enable / disable colors in the output (reporters and logs)
-        colors         : true,
+        colors     : true,
 
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel       : config.LOG_INFO,
+        logLevel   : config.LOG_INFO,
+
+        loggers        : [
+            {type : 'console'}
+        ],
 
         // enable / disable watching file and executing tests whenever any file changes
         autoWatch      : true,
@@ -43,10 +51,10 @@ module.exports = function(config){
         // - Safari (only Mac)
         // - PhantomJS
         // - IE (only Windows)
-        browsers       : ['PhantomJS'],
+        browsers       : ['PhantomJS', 'Chrome', 'Firefox', 'Opera'],
 
         // If browser does not capture in given timeout [ms], kill it
-        captureTimeout : 60000,
+        captureTimeout : 10000,
 
         // Continuous Integration mode
         // if true, it capture browsers, run tests and exit
