6d4dee2234311e03e06baaff0b69b5e190be5522
Luka Sakun <lukasakun@gmail.com>
2013-07-31 21:03:40 +0300

    0.0.1

--- scripts/extend.js
+++ scripts/extend.js
@@ -1,3 +1,20 @@
 function extend(P, px, sx){
+  var E = function() { this.constructor(arguments)},
+    buf = function() {},
+    prop;
 
+  buf.prototype = P.prototype;
+  E.prototype = new buf;
+
+  for(prop in P) {
+    E[prop] = P[prop];
+  }
+  for(prop in px) {
+    E.prototype[prop] = px[prop];
+  }
+  for(prop in sx) {
+    E[prop] = sx[prop];
+  }
+
+  return E;
 }
