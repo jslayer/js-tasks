6706418f13840c0bbe048901dec82eb1751d7cc4
Kiira02 <Kiira005@gmail.com>
2013-08-02 12:04:00 -0700

    Update extend.js

--- scripts/extend.js
+++ scripts/extend.js
@@ -1,16 +1,12 @@
 function extend(P, px, sx){
   var obj; 
-  obj.prototype = new P();
-  obj = function () {
-	this.initialize();
-  };
   obj.prototype.initialize = function () {
-	this.value = px.value;
+    this.value = px.value;
+    obj.prototype.methodA = P.methodA;
+    obj.prototype.methodB = px.methodB;
+    obj.sMethodA = P.sMethodA;  
+    obj.sMethodB = sx.sMethodB;
   };
-  obj.prototype.methodA = P.methodA;
-  obj.prototype.methodB = px.methodB;
-  obj.sMethodA = P.sMethodA;  
-  obj.sMethodB = sx.sMethodB;  
   
 return obj;
 }
