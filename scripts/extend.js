70dfd7ed4be285bd64bd11d834d7501dcd8710ec
Denis Ivanchenko <denis.ivanchenko@volcanoideas.com>
2013-08-06 13:08:16 +0300

    task 0.0.1

--- scripts/extend.js
+++ scripts/extend.js
@@ -1,3 +1,17 @@
 function extend(P, px, sx){
 
+    var child = function() {this.constructor.prototype.constructor.call(this)};
+    child.prototype = new P();
+
+    for (property in P) {
+        child[property] = P[property];
+    }
+    for (property in px) {
+        child.prototype[property] = px[property];
+    }
+    for (property in sx) {
+        child[property] = sx[property];
+    }
+
+    return child;
 }
