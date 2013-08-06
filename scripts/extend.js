08813762fcc22b9dbd412ccb47ffd8d99a1cd343
Denis Ivanchenko <denis.ivanchenko@volcanoideas.com>
2013-08-06 16:19:46 +0300

    task v0.0.1 v2

--- scripts/extend.js
+++ scripts/extend.js
@@ -1,13 +1,35 @@
 function extend(P, px, sx){
 
-    var child = function() {this.constructor.prototype.constructor.call(this)};
-    child.prototype = new P();
+    function extendInner(Child, Parent) {
+        var F = function() {  };
+        F.prototype = Parent.prototype;
+        Child.prototype = new F();
+        Child.prototype.constructor = Child;
+        Child.superclass = Parent.prototype;
+
+
+    }
+
+
+    var child = function() {child.superclass.constructor.apply(this, arguments)};
+    extendInner(child, P);
 
     for (property in P) {
         child[property] = P[property];
     }
     for (property in px) {
-        child.prototype[property] = px[property];
+        switch (property) {
+            case 'constructor':
+                var oldProto = child.prototype;
+                child = px[property];
+                child.prototype = oldProto;
+                child.prototype.constructor = child;
+                break
+            default:
+                child.prototype[property] = px[property];
+                break
+        }
+
     }
     for (property in sx) {
         child[property] = sx[property];
