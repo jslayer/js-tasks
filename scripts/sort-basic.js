8e4090dd5a87fde9c3b35025271153b3fed423a4
Kiira02 <Kiira005@gmail.com>
2013-08-08 00:11:36 +0300

    Update sort-basic.js

--- scripts/sort-basic.js
+++ scripts/sort-basic.js
@@ -1,1 +1,21 @@
-//todo - implement
+Array.prototype.sort = function(predicate) {
+    var result = [],
+        dirty = true;
+
+    while (dirty) {
+        dirty = false;
+        for (var i = 0; i < this.length - 1; i++) {
+            var equal = predicate(this[i], this[i + 1]);
+            if (!equal) continue;
+            {
+                if (equal > 0) {
+                    var temp = this[i + 1];
+                    this[i + 1] = this[i];
+                    this[i] = temp;
+                    dirty = true;
+                } else {
+                }
+            }
+        }
+    }
+};
