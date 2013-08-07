1ebea66c923261270a13cb3129d9e59bc1d791bb
Kiira02 <Kiira005@gmail.com>
2013-08-08 00:12:27 +0300

    Update strings-basic.js

--- scripts/strings-basic.js
+++ scripts/strings-basic.js
@@ -1,31 +1,67 @@
-var StringsBasic = {
-    /**
-     * @param {string} str
-     * @param {string} sub
-     * @returns {number}
-     */
-    subCount : function(str, sub) {},
+function testString() {
+    var StringsBasic = {
+        /**
+    * @param {string} str
+    * @param {string} sub
+    * @returns {number}
+    */
+        subCount: function (str, sub) {
+            return str.toLowerCase().split(sub.toLowerCase()).length - 1;
+        },
 
-    /**
-     * @param {string} str
-     * @param {string} sub
-     * @returns {Array,<string>}
-     */
-    wordsContains : function(str, sub) {},
-    /**
-     * @param {string} str
-     * @param {string} sub
-     * @returns {Array.<string>}
-     */
-    wordsNotContains : function(str, sub) {},
-    /**
-     * @param {string} str
-     * @returns {Array.<string>}
-     */
-    wordsBeginCapital : function(str) {},
-    /**
-     * @param {string} str
-     * @returns {boolean}
-     */
-    isPalindrome : function(str) {}
-};
+        /**
+    * @param {string} str
+    * @param {string} sub
+    * @returns {Array,<string>}
+    */
+        wordsContains: function (str, sub) {
+            var array = str.replace(/[&\/|\\#,+()$~%.'":*?<>{}]/g, ' ').split(" ");
+            var result = [];
+            for (var i in array) {
+                if (array[i].toLowerCase().search(sub.toLowerCase()) != -1) {
+                    result.push(array[i]);
+                }
+            }
+            return result;
+        },
+        /**
+    * @param {string} str
+    * @param {string} sub
+    * @returns {Array.<string>}
+    */
+        wordsNotContains: function (str, sub) {
+            var array = str.replace(/[&\/|\\#,+()$~%.'":*?<>{}]/g, ' ').split(" ");
+            var result = [];
+            for (var i in array) {
+                if (array[i].toLowerCase().search(sub.toLowerCase()) == -1) {
+                    result.push(array[i]);
+                }
+            }
+            return result;
+        },
+        /**
+    * @param {string} str
+    * @returns {Array.<string>}
+    */
+        wordsBeginCapital: function (str) {
+            var array = str.replace(/[&\/|\\#,+()$~%.'":*?<>{}0-9]/g, ' ').split(" ");
+            var result = [];
+            for (var i in array) {
+                var letter = array[i].substr(0, 1);
+                if (letter == letter.toUpperCase() && letter != '') {
+                    result.push(array[i]);
+                }
+            }
+            return result;
+        },
+        /**
+    * @param {string} str
+    * @returns {boolean}
+    */
+        isPalindrome: function (str) {
+            if (str.toLowerCase() == str.split('').reverse().join('').toLowerCase()) {
+                return true;
+            }
+            return false;
+        }
+    };
