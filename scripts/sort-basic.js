2f5d6fb56299566c948e5705de3f178121339e59
Kiira02 <Kiira005@gmail.com>
2013-08-08 02:46:04 +0300

    Update sort-basic.js

--- scripts/sort-basic.js
+++ scripts/sort-basic.js
@@ -1,25 +1,28 @@
-/*Array.prototype.sort = function(predicate) {
-    var dirty = true;
-
-    if (!predicate) {
-        predicate = function(a, b) { return a - b; };
-    }
-
-    while (dirty) {
-        dirty = false;
-        for (var i = 0; i < this.length - 1; i++) {
-            var equal = predicate(this[i], this[i + 1]);
-            if (!equal) continue;
-            {
-                if (equal > 0) {
-                    var temp = this[i + 1];
-                    this[i + 1] = this[i];
-                    this[i] = temp;
-                    dirty = true;
-                } 
+function () {
+    "use strict";
+    Array.prototype.sort = function(predicate) {
+        var dirty = true;
+    
+        if (!predicate) {
+            predicate = function(a, b) { return a - b; };
+        }
+    
+        while (dirty) {
+            dirty = false;
+            for (var i = 0; i < this.length - 1; i++) {
+                var equal = predicate(this[i], this[i + 1]);
+                if (!equal) continue;
+                {
+                    if (equal > 0) {
+                        var temp = this[i + 1];
+                        this[i + 1] = this[i];
+                        this[i] = temp;
+                        dirty = true;
+                    } 
+                }
             }
         }
-    }
-
-    return this;
-};*/
+    
+        return this;
+    };
+}
