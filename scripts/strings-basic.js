53378259c881119fcb9a2d7405bd1e3a00f1171a
Kiira02 <Kiira005@gmail.com>
2013-08-08 03:44:54 +0300

    Update strings-basic.js

--- scripts/strings-basic.js
+++ scripts/strings-basic.js
@@ -14,7 +14,7 @@ var StringsBasic = {
 * @returns {Array,<string>}
 */
     wordsContains: function (str, sub) {
-        var array = str.replace(/[&\/|\\#,+()$~%.'":*?!<>{}]_/g, " ").split(" ");
+        var array = str.replace(/[&\/|\\#,+()$~%.'":*?!<>{}_]/g, " ").split(" ");
         var result = [];
         for (var i in array) {
             if (array[i].toLowerCase().search(sub.toLowerCase()) != -1) {
@@ -29,7 +29,7 @@ var StringsBasic = {
 * @returns {Array.<string>}
 */
     wordsNotContains: function (str, sub) {
-        var array = str.replace(/[&\/|\\#,+()$~%.'":*?!<>{}]_/g, " ").split(" ");
+        var array = str.replace(/[&\/|\\#,+()$~%.'":*?!<>{}_]/g, " ").split(" ");
         var result = [];
         for (var i in array) {
             if (array[i].toLowerCase().search(sub.toLowerCase()) == -1) {
