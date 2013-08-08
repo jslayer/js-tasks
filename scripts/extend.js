3c6ac294910db0b765d99d99a741cde29f68fa1b
Kiira02 <Kiira005@gmail.com>
2013-08-08 04:39:20 +0300

    Update extend.js

--- scripts/extend.js
+++ scripts/extend.js
@@ -1,5 +1,7 @@
 function extend(P, px, sx){
-        var obj = function () {};
+        var obj = function () {
+            P.call(this);
+        };
 
         obj.prototype = new P();
 
@@ -19,11 +21,11 @@ function extend(P, px, sx){
             obj[idx] = sx[idx];
         }
 
-        for (var item in P) {
+        /*for (var item in P) {
             if (obj[item] == undefined) {
                 obj[item] = P[item];
             }
-        }
+        }*/
 
         return obj;
 }
