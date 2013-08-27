35bdf5250c0c126264ce8af823d35111e9417faa
AlexKotov <alexcranston55@gmail.com>
2013-08-27 22:20:35 +0300

    Update extend.js

--- scripts/extend.js
+++ scripts/extend.js
@@ -1,8 +1,3 @@
 function extend(P, px, sx) {
-  return function() {
-       this.initialize = px.initialize;   
-       this.initialize();
-       this.MethodB = px.MethodB;
-       this.sMethodB = sx.sMethodB;
-  };
+  
 }
