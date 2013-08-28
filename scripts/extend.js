9cb89be2267aa68d25678818da64fe6030089199
AlexKotov <alexcranston55@gmail.com>
2013-08-28 16:21:51 +0300

    Update extend.js

--- scripts/extend.js
+++ scripts/extend.js
@@ -1,3 +1,12 @@
 function extend(P, px, sx) {
+  var obj = function() {};
+  obj.prototype = new P;
   
+  for(var i in px)
+    obj.prototype[i] = px[i];
+    
+  for(var id in sx)
+    obj[i] = sx[i];
+    
+  return obj;
 }
